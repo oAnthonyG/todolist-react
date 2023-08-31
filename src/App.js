0be5c22d67ac6e8c99b49e0d727f7c2de986d3e9
anthony <anthonytguilherme@hotmail.com>
2023-08-31 19:20:45 -0300

    add useState component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import {BsTrash, BsBookmarkCheck, BsBookmarkCheckFill} from 'react-icons/bs';
 const API = "http://localhost:5000";
 
 function App() {
+  const [title, setTitle] = useState("")
+  const [time, setTime] = useState("")
+  const [todos, setTodos] = useState([])
+  const [loading, setLoading] = useState(false)
+
   return (
     <div className="App">
       todo
